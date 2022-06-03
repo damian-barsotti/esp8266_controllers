eaf0ff8c9c08b031a04b2994f4abec4dd5db1b70
Damian Barsotti <damian@famaf.unc.edu.ar>
2022-06-03 19:26:18 -0300

    Fix error flag

--- HTReader.cpp
+++ HTReader.cpp
@@ -11,7 +11,7 @@ HTReader::HTReader(uint8_t pin, uint8_t type, uint16_t sleeping_time, uint16_t r
             dht.begin();
             _last_sensor_read_time = 0;
             _last_avg_sensor_read_time = 0;
-            _error = _read_sensors(_ac_t, _ac_h);
+            _error = ! _read_sensors(_ac_t, _ac_h);
             _n_sensor_reads = 1;
             _t = _ac_t;
             _h = _ac_h;
