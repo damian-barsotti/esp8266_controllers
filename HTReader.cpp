260c2570c6680b3e5b7fae8785c9a58b7475270d
BuildTools <damian@famaf.unc.edu.ar>
2024-05-26 20:52:15 -0300

    Indent

--- HTReader.cpp
+++ HTReader.cpp
@@ -4,53 +4,61 @@
 #define SENSOR_DELAY 2000 // In msec
 
 HTReader::HTReader(uint8_t pin, uint8_t type, uint16_t sleeping_time, uint16_t n_avg,
-    float temp_slope, float temp_shift, float humid_slope, float humid_shift)
-     : dht(pin, type), _sleeping_time(sleeping_time), _n_avg(n_avg),
-        _temp_slope(temp_slope), _temp_shift(temp_shift), 
-        _humid_slope(humid_slope), _humid_shift(humid_shift){
-            dht.begin();
-            reset();
-            }
+                   float temp_slope, float temp_shift, float humid_slope, float humid_shift)
+    : dht(pin, type), _sleeping_time(sleeping_time), _n_avg(n_avg),
+      _temp_slope(temp_slope), _temp_shift(temp_shift),
+      _humid_slope(humid_slope), _humid_shift(humid_shift)
+{
+    dht.begin();
+    reset();
+}
 
-HTReader::HTReader(uint8_t pin, uint8_t type, 
-    float temp_slope, float temp_shift, float humid_slope, float humid_shift)
-     : dht(pin, type), _sleeping_time(0), _n_avg(0),
-        _temp_slope(temp_slope), _temp_shift(temp_shift), 
-        _humid_slope(humid_slope), _humid_shift(humid_shift){
-            dht.begin();
-            reset();
-            }
+HTReader::HTReader(uint8_t pin, uint8_t type,
+                   float temp_slope, float temp_shift, float humid_slope, float humid_shift)
+    : dht(pin, type), _sleeping_time(0), _n_avg(0),
+      _temp_slope(temp_slope), _temp_shift(temp_shift),
+      _humid_slope(humid_slope), _humid_shift(humid_shift)
+{
+    dht.begin();
+    reset();
+}
 
-bool HTReader::reset(){
-            _last_sensor_read_time = 0;
-            _error = ! _read_sensors(_ac_t, _ac_h);
-            _n_sensor_reads = 1;
-            _t = _ac_t;
-            _h = _ac_h;
-            return ! _error;
-            }
+bool HTReader::reset()
+{
+    _last_sensor_read_time = 0;
+    _error = !_read_sensors(_ac_t, _ac_h);
+    _n_sensor_reads = 1;
+    _t = _ac_t;
+    _h = _ac_h;
+    return !_error;
+}
 
 // To run inside loop.
 // Return true if new data ready.
-bool HTReader::beginLoop(){
+bool HTReader::beginLoop()
+{
     float t, h;
 
     // Not to use with deep sleep (snd constructor)
-    if (_n_avg == 0) {
+    if (_n_avg == 0)
+    {
         _error = true;
         return false;
     }
     _error = false;
 
-    if (_n_sensor_reads >= _n_avg){
+    if (_n_sensor_reads >= _n_avg)
+    {
         _ac_t = 0;
         _ac_h = 0;
         _n_sensor_reads = 0;
     }
 
-    if (_last_sensor_read_time >= delay_ms()){
-        _error = ! _read_sensors(t, h);
-        if (!_error){
+    if (_last_sensor_read_time >= delay_ms())
+    {
+        _error = !_read_sensors(t, h);
+        if (!_error)
+        {
             _ac_t += t;
             _ac_h += h;
             _n_sensor_reads++;
@@ -61,40 +69,47 @@ bool HTReader::beginLoop(){
         _last_sensor_read_time = 0;
     }
 
-    _last_sensor_read_time += _sleeping_time; 
+    _last_sensor_read_time += _sleeping_time;
 
     return _n_sensor_reads >= _n_avg;
 }
 
-float HTReader::getTemp(){
+float HTReader::getTemp()
+{
     return _t;
 }
 
-float HTReader::getHumid(){
+float HTReader::getHumid()
+{
     return _h;
 }
 
-bool HTReader::error(){
+bool HTReader::error()
+{
     return _error;
 }
 
-uint16_t HTReader::delay_ms(){ return SENSOR_DELAY; }
+uint16_t HTReader::delay_ms() { return SENSOR_DELAY; }
 
-bool HTReader::_read_sensors(float &t, float &h){
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  // Read temperature as Celsius (the default)
-  t = dht.readTemperature();
-  h = dht.readHumidity();
+bool HTReader::_read_sensors(float &t, float &h)
+{
+    // Reading temperature or humidity takes about 250 milliseconds!
+    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+    // Read temperature as Celsius (the default)
+    t = dht.readTemperature();
+    h = dht.readHumidity();
 
-  if (isnan(h) || isnan(t)) {
-      h = 0.0; t = 0.0; 
-      return false;
-  } else {
-      // adjust DHT11
-      h = _humid_slope * h + _humid_shift;
-      t = _temp_slope * t + _temp_shift; // Read temperature as C
-      return true;
-  }
+    if (isnan(h) || isnan(t))
+    {
+        h = 0.0;
+        t = 0.0;
+        return false;
+    }
+    else
+    {
+        // adjust DHT11
+        h = _humid_slope * h + _humid_shift;
+        t = _temp_slope * t + _temp_shift; // Read temperature as C
+        return true;
+    }
 }
-
--- HTReader.h
+++ HTReader.h
@@ -5,30 +5,32 @@
 
 #include <DHT.h>
 
-class HTReader {
-    public:
-    HTReader(uint8_t pin, uint8_t type, uint16_t sleeping_time, uint16_t n_avg, 
-        float temp_slope, float temp_shift, float humid_slope, float humid_shift);
-    // To use with deep sleep without beginLoop  
-    HTReader(uint8_t pin, uint8_t type, 
-        float temp_slope, float temp_shift, float humid_slope, float humid_shift);
+class HTReader
+{
+public:
+    HTReader(uint8_t pin, uint8_t type, uint16_t sleeping_time, uint16_t n_avg,
+             float temp_slope, float temp_shift, float humid_slope, float humid_shift);
+    // To use with deep sleep without beginLoop
+    HTReader(uint8_t pin, uint8_t type,
+             float temp_slope, float temp_shift, float humid_slope, float humid_shift);
     bool reset();
     bool beginLoop();
     float getTemp();
     float getHumid();
     bool error();
     uint16_t delay_ms();
-    private:
-        DHT dht;
-        const uint16_t _sleeping_time;
-        const uint16_t _n_avg;
-        const float _temp_slope, _temp_shift, _humid_slope, _humid_shift;
-        int _last_sensor_read_time;
-        int _n_sensor_reads;
-        float _ac_t, _ac_h;
-        float _t, _h;
-        bool _error;
-        bool _read_sensors(float &t, float &h);
+
+private:
+    DHT dht;
+    const uint16_t _sleeping_time;
+    const uint16_t _n_avg;
+    const float _temp_slope, _temp_shift, _humid_slope, _humid_shift;
+    int _last_sensor_read_time;
+    int _n_sensor_reads;
+    float _ac_t, _ac_h;
+    float _t, _h;
+    bool _error;
+    bool _read_sensors(float &t, float &h);
 };
 
 #endif
